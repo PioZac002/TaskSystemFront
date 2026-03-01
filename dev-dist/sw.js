91c515a36c947e9dbbf2c22cf6757bcfe64c0524
Piotr Zaćmiński <piotrek.zacminski2002@gmail.com>
2026-03-01 19:04:49 +0100

    need to fix branch problem

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.943mg7chf8"
+    "revision": "0.l2djj1572h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
