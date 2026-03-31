cf618577a1044ef83e2645ca0cbe5b6db3c29ead
Piotr Zaćmiński <piotrek.zacminski2002@gmail.com>
2026-03-31 02:43:35 +0200

    need this to trigger deploy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q3ka1eihg8o"
+    "revision": "0.c2p1akpflho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/features/landing/LandingPage.jsx
+++ src/features/landing/LandingPage.jsx
@@ -595,7 +595,7 @@ export default function LandingPage() {
                                 <span className="text-lg font-bold text-slate-900 dark:text-white">TaskSystem</span>
                             </div>
                             <p className="text-sm text-slate-600 dark:text-slate-400">
-                                Testing CI/CD lol
+                                ok :D
                             </p>
                         </div>
                         <div>
